38655851bd99b3ea72c126dbc2dda995b6b74f72
Obriv Mostov <mostobriv@MacBook-Pro-Obriv.local>
2018-09-09 01:11:56 +0700

    replaced fucking tabs

--- echo_video_direct/bootloader.asm
+++ echo_video_direct/bootloader.asm
@@ -5,75 +5,75 @@ video_memory equ 0B800h
 
 
 start:
-	
-	xor ax, ax
-	mov ds, ax
+    
+    xor ax, ax
+    mov ds, ax
 
-	mov ss, ax
-	mov sp, 0FFFEh
+    mov ss, ax
+    mov sp, 0FFFEh
 
-	call echo
+    call echo
 
-	jmp $
+    jmp $
 
 
 echo:
-	pusha
+    pusha
 
-	mov ax, video_memory
-	mov es, ax
+    mov ax, video_memory
+    mov es, ax
 
-	mov cx, 07D0h
-	xor di, di
-	.clear_screen:
-		mov [es:di], word 020h
-		add edi, 2
-		loop .clear_screen
+    mov cx, 07D0h
+    xor di, di
+    .clear_screen:
+        mov [es:di], word 020h
+        add edi, 2
+        loop .clear_screen
 
-	mov cx, ascii_art.len
-	shr cx, 1
+    mov cx, ascii_art.len
+    shr cx, 1
 
-	mov si, ascii_art
-	xor di, di
-	.loopy_loop:
-		mov ax, [si]
-		
-		push cx
-		mov cx, 16
-		.nested_loop:
-			test ax, 08000h
-			jz .print_space
-			mov [es:di], byte 'X'
-			inc di
-			mov [es:di], byte 2
-			jmp .kek
-			.print_space:
-				mov [es:di], byte ' '
-				inc di
-				mov [es:di], byte 0
-			.kek:
-			shl ax, 1
-			inc di
-			loop .nested_loop
-		pop cx
+    mov si, ascii_art
+    xor di, di
+    .loopy_loop:
+        mov ax, [si]
+        
+        push cx
+        mov cx, 16
+        .nested_loop:
+            test ax, 08000h
+            jz .print_space
+            mov [es:di], byte 'X'
+            inc di
+            mov [es:di], byte 2
+            jmp .kek
+            .print_space:
+                mov [es:di], byte ' '
+                inc di
+                mov [es:di], byte 0
+            .kek:
+            shl ax, 1
+            inc di
+            loop .nested_loop
+        pop cx
 
-		pusha
+        pusha
 
-		; little sleepy tho
-		mov al, 0
-		mov ah, 86h
-		mov cx, 1
-		mov dx, 2
-		int 15h
+        ; little sleepy tho
+        mov al, 0
+        mov ah, 86h
+        mov cx, 1
+        mov dx, 2
+        int 15h
 
-		popa
+        popa
 
-		add si, 2
-		loop .loopy_loop
+        add si, 2
+        loop .loopy_loop
 
-	popa
+    popa
 
-	ret
+    ret
 
 
 ascii_art dw 0, 1, 65535, 49152, 0, 0, 15, 49153, 64512, 0, 0, 120, 0, 3840, 0, 0, 224, 0, 192, 0, 0, 1728, 0, 432, 0, 0, 1632, 0, 432, 0, 0, 1632, 0, 816, 0, 0, 867, 64543, 58208, 0, 1, 49255, 63503, 63873, 57344, 3, 57543, 61895, 61827, 61440, 6, 12481, 50145, 49542, 12288, 14, 7776, 2032, 828, 14336, 31, 61567, 34800, 65283, 64512, 7, 48647, 49152, 60447, 47104, 0, 2023, 12289, 40188, 0, 0, 126, 57341, 48896, 0, 0, 6, 54613, 22528, 0, 0, 502, 16382, 14272, 0, 1, 65411, 0, 24703, 49152, 3, 31, 49153, 65024, 24576, 1, 49648, 32767, 1985, 49152, 0, 25472, 4088, 227, 0, 0, 26112, 0, 51, 0, 0, 7680, 0, 15, 0
