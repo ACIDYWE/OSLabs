de557c271cf7af26fd64154af8ee82cc4ae25ca8
Obriv Mostov <mostobriv@MacBook-Pro-Obriv.local>
2019-01-09 16:45:35 +0700

    fixes

--- protected_mode/run.sh
+++ protected_mode/run.sh
@@ -5,5 +5,5 @@ if [ "$#" -ne 0 ]; then
     qemu-system-i386 -s -S -monitor stdio -drive file=disk.img,format=raw -m 32
 else
     # NON DEBUG
-    qemu-system-i386 -monitor stdio -drive file=disk.img,format=raw -m 128
+    qemu-system-i386 -monitor stdio -drive file=disk.img,format=raw -m 32
 fi
